9e8fbc12736799611e8f9a0d59f5777a6251c6c8
Mateo_Liberatore <liberatoremate98@gmail.com>
2025-11-21 01:10:57 -0300

    fix: Corregir configuración

--- postcss.config.js
+++ postcss.config.js
@@ -1,9 +1,9 @@
 // postcss.config.js
 module.exports = {
   plugins: {
-    // 1. CORRECCIÓN: El nombre del plugin es 'tailwindcss', no '@tailwindcss/postcss'
+
     tailwindcss: {},
-    // 2. Necesario para compatibilidad de CSS en navegadores
+    
     autoprefixer: {}, 
   },
 };
