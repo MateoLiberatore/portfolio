2afdae7d7740f053dc9a1eb4556cf88e5b169512
Mateo_Liberatore <83372310+MateoLiberatore@users.noreply.github.com>
2025-11-20 23:49:40 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,11 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-
+  base: '/portfolio/', 
+  
   plugins: [
     tailwindcss(),
+    
     react({
       babel: {
         plugins: [
